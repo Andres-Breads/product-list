558727e6a4c42cf4bb09dd7d8a3bf027faf8e7f9
Carlos Aucaruri <programador@laborfinanciera.com>
2023-05-01 04:05:05 -0500

    Added CustomText that uses Avenir

---
+++ src/components/CustomText.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text, StyleSheet, TextStyle } from 'react-native';
+
+type CustomTextProps = {
+  children: React.ReactNode;
+  style?: TextStyle;
+}
+
+const CustomText: React.FC<CustomTextProps> = ({ children, style }) => {
+  return (
+    <Text style={[styles.text, style]}>{children}</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "Avenir-Roman",
+  },
+});
+
+export default CustomText;
