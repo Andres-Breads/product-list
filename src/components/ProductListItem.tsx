fdcb0568798a947fd401663dc37537a535c6c780
Carlos Aucaruri <programador@laborfinanciera.com>
2023-05-01 04:09:45 -0500

    ProductListItem updated

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
+import CustomText from './CustomText';
 import { ProductType } from '../types';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 type ProductListItemProps = {
   product: ProductType;
@@ -9,20 +11,66 @@ type ProductListItemProps = {
 };
 
 const ProductListItem: React.FC<ProductListItemProps> = ({ product, onPress }) => (
-  <TouchableOpacity style={styles.productItem} onPress={onPress}>
-    <Text>{product.product}</Text>
+  <TouchableOpacity style={styles.container} onPress={onPress}>
+    <Image style={styles.image} source={{ uri: product.image }} />
+    <View style={styles.details}>
+      <CustomText style={styles.product}>{product.product}</CustomText>
+      <CustomText style={styles.createdAt}>
+        {new Date(product.createdAt).toLocaleDateString('es-MX', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })}
+      </CustomText>
+    </View>
+    <View style={styles.points}>
+      <CustomText style={styles.pointsText}>
+        {product.is_redemption ? '-' : '+'}{product.points}
+      </CustomText>
+      <Icon name='chevron-right' size={18} color='#666' style={styles.arrow} />
+    </View>
   </TouchableOpacity>
 )
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingVertical: 4,
   },
-  productItem: {
-    padding: 16
-  }
+  image: {
+    width: 55,
+    height: 55,
+    marginRight: 16,
+    borderRadius: 10,
+  },
+  details: {
+    flex: 4,
+  },
+  product: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 7,
+  },
+  points: {
+    alignSelf: 'center',
+    flex: 2,
+    flexDirection: 'row',
+  },
+  pointsText: {
+    fontSize: 16,
+    color: '#666',
+  },
+  arrowContainer: {
+    
+  },
+  arrow: {
+    alignSelf: 'center',
+    marginLeft: 'auto',
+  },
+  createdAt: {
+    fontSize: 14,
+    color: '#666',
+  },
 });
 
 export default ProductListItem;
